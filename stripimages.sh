6a9990de02f5c79282f5b909a3ed864425bf6874
Ross Mounce <Ross.Mounce@gmail.com>
2012-06-14 14:18:29 +0100

    pdf image stripper

---
+++ stripimages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Usage: attempt to make a plaintext conversion copy of all PDFs in all subfolders (maxdepth 1 folder down) of the working directory
+
+# Turning on the nullglob shell option
+shopt -s nullglob
+
+# Make list of all subfolders in working directory, save as pwd.txt
+find * -maxdepth 0 -type d -exec bash -c "cd \"{}\"; pwd" \;  > pwd.txt
+
+# Loop through pwd cd'ing into each directory then pdftotext all PDFs within each subdirectory
+for i in $(cat pwd.txt); do
+  cd $i 
+  for f in *.pdf
+	do
+	echo "stripping figures out of $f"
+        pdfimages -j "$f" "$f" 
+	done
+done
