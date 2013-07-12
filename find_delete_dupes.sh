f5575879b0757dc13df5b3749a9a23a2d3c48d02
Ross Mounce <ross.mounce@gmail.com>
2013-07-12 12:57:21 +0100

    Update find_delete_dupes.sh

--- find_delete_dupes.sh
+++ find_delete_dupes.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 fdupes -fr . > dupes.txt;
 xargs -a dupes.txt -d'\n' rm -f;
 # -r for recursive search, -f omit first duplicate, -1 each set of matches on same line, -S show size of files
